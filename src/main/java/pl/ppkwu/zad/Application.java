71d8e5148035d347737b94dd3072f125704c8c01
pawełek <p.dawiduk@gmail.com>
2016-10-25 11:10:55 +0200

    Created object

--- src/main/java/pl/ppkwu/zad/Application.java
+++ src/main/java/pl/ppkwu/zad/Application.java
@@ -1,8 +1,20 @@
 package pl.ppkwu.zad;
 
+import pl.ppkwu.zad.api.FileLibrary;
+import pl.ppkwu.zad.impl.FileLibraryImpl;
+import pl.ppkwu.zad.impl.IOUtils;
+import pl.ppkwu.zad.impl.RetryCallbackImpl;
+
 public class Application {
 
+    private static FileLibrary fileLibrary;
+    private static RetryCallbackImpl retryCallback;
+
     public static void main(String[] args) {
+        fileLibrary = new FileLibraryImpl();
+        retryCallback = new RetryCallbackImpl();
+
+        fileLibrary.setRetryCallback(retryCallback);
 
     }
 }
